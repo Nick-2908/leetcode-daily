c0d7d872b31c733ae2d6b7349978f6cc5679e66f
Nikhita <nikhitauppar8@gmail.com>
2026-01-12 20:42:46 +0530

    Jan 12

---
+++ Jan/asteroid.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack=new Stack<>();
+
+        for (int curr:asteroids) {
+            boolean destroyed=false;
+
+            while(!stack.isEmpty() && stack.peek()>0 && curr<0){
+                if(stack.peek()<-curr) {
+                    stack.pop();
+                }else if(stack.peek()==-curr){
+                    stack.pop();
+                    destroyed=true;
+                    break;
+                }else{
+                    destroyed=true;
+                    break;
+                }
+            }
+
+            if(!destroyed){
+                stack.push(curr);
+            }
+        }
+
+        int[] ans=new int[stack.size()];
+        for (int i=ans.length-1;i>=0;i--){
+            ans[i]=stack.pop();
+        }
+
+        return ans;
+    }
+}
